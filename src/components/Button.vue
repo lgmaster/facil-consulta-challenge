84011e7caf57d5fcda47b8045b78038ea802a4d2
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-08 20:15:48 -0300

    feat: created button component

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <FormulateInput class="form__button" type="submit" :label="buttontext" />
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: ["buttontext"],
+};
+</script>
+
+<style lang="scss">
+.form__button button {
+  width: 100%;
+  height: toRem(40px);
+  background: $primary-0;
+  color: $secondary-0;
+  text-transform: uppercase;
+  border-radius: toRem(12px);
+  border: none;
+
+  span {
+    font-family: $Comfortaa;
+    font-weight: 700;
+  }
+}
+</style>
