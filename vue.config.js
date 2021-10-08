0934698a8476216324f9651f757060552afe8a30
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-08 16:01:53 -0300

    style: add global style and import variables and functions

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: "@import \"~@/scss/_variables.scss\";",
+        prependData: "@import \"~@/scss/_global.scss\";",
       },
     },
   },
