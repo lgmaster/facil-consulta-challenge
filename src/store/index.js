481523eeeb2bdec39414cc21613ebb2255baf8bc
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-08 20:17:49 -0300

    feat: created vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    step: 1,
+  },
+  getters: {
+    getActiveStep(state) {
+      return state.step;
+    },
   },
   mutations: {
+    incrementStep(state) {
+      state.step += 1;
+    },
   },
   actions: {
   },
