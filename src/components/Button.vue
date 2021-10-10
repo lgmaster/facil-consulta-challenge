bbcf2a327d86bf030c8884c2e5a2c5031307fd5e
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-10 16:20:18 -0300

    refactor: receive color and text in props

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,19 +1,32 @@
 <template>
-  <FormulateInput class="form__button" type="submit" :label="buttontext" />
+  <FormulateInput
+    class="form__button"
+    type="submit"
+    :label="buttontext"
+    :class="color"
+  />
 </template>
 
 <script>
 export default {
   name: "Button",
-  props: ["buttontext"],
+  props: ["buttontext", "color"],
 };
 </script>
 
 <style lang="scss">
+.form__button.primary-0 button {
+  background: $primary-0;
+}
+
+.form__button.cta-0 button {
+  background: $cta-0;
+  color: $primary-0;
+}
+
 .form__button button {
   width: 100%;
   height: toRem(40px);
-  background: $primary-0;
   color: $secondary-0;
   text-transform: uppercase;
   border-radius: toRem(12px);
