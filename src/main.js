23668cf24b6464fa850fc7d3573c768a731358d2
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-10 16:44:10 -0300

    feat: add unicon setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
+import Unicon from "vue-unicons/dist/vue-unicons-vue2.umd";
+import { uniAngleLeftB } from "vue-unicons/dist/icons";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+Unicon.add([uniAngleLeftB]);
+Vue.use(Unicon);
 
 Vue.config.productionTip = false;
 
