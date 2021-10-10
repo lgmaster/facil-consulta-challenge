3a43b7e3ffb6b8c8febc91883992dd39f1fbe3a3
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-10 14:30:07 -0300

    feat: created methods to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,22 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     step: 1,
+    formData: {},
   },
   getters: {
     getActiveStep(state) {
       return state.step;
     },
+    getFormData(state) {
+      return state.formData.data;
+    },
   },
   mutations: {
     incrementStep(state) {
       state.step += 1;
+    },
+    saveFormData(state, data) {
+      state.formData = { ...state.formData, data };
     },
   },
   actions: {
