8f57fa38baa527997b53a59a3a95aef1d266effd
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-10 16:22:02 -0300

    feat: add new components to render by step

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,6 +2,8 @@
   <div class="col">
     <FormStep1 v-if="step == 1" />
     <FormStep2 v-if="step == 2" />
+    <Revision v-if="step == 3" />
+    <Completed v-if="step == 4" />
   </div>
 </template>
 
@@ -9,9 +11,16 @@
 import { mapGetters } from "vuex";
 import FormStep1 from "./FormStep1.vue";
 import FormStep2 from "./FormStep2.vue";
+import Revision from "./Revision.vue";
+import Completed from "./Completed.vue";
 
 export default {
-  components: { FormStep1, FormStep2 },
+  components: {
+    FormStep1,
+    FormStep2,
+    Revision,
+    Completed,
+  },
   computed: {
     ...mapGetters({
       step: "getActiveStep",
