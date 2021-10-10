e1e3700e91520fc7f53d82a3955a4dcebe596d8b
Luiz Gustavo Amorim <lgdeveloper@outlook.com>
2021-10-10 16:43:23 -0300

    feat: add button to back steps

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <template>
   <main id="app">
+    <nav
+      v-if="step > 1 && step < 4"
+      class="container d-flex flex-1 align-items-center"
+    >
+      <unicon
+        name="angle-left-b"
+        fill="black"
+        class="flex-grow-1"
+        @click="handleClickButton"
+      ></unicon>
+    </nav>
     <router-view />
   </main>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({
+      step: "getActiveStep",
+    }),
+  },
+  methods: {
+    handleClickButton() {
+      this.$store.commit("decrementStep");
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -10,5 +38,13 @@
   height: 100%;
   width: 100%;
   min-height: 100vh;
+
+  nav {
+    height: toRem(68px);
+
+    .unicon {
+      cursor: pointer;
+    }
+  }
 }
 </style>
